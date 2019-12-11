bdd97757c3ff0dd448253d80f7acaf6bbd6e4b36
Anders Nordal Weiskvist <ande6059@edu.eal.dk>
2019-12-11 11:27:21 +0100

    Add project files.

---
+++ SjovMedDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SjovMedDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
